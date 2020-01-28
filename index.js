91cf11586a6007212ca8fa8f824fd87cb64907ad
Ashna Shah-Grover <as19@williams.edu>
2020-01-27 19:07:26 -0500

    Half of the Linked List Lab

---
+++ index.js
@@ -0,0 +1,48 @@
+let firstNode = {name: 'Alexandra', next: 108}
+let secondNode = {name: 'Kirstin', next: 917}
+let thirdNode = {name: 'Juliet', next: 389}
+let fourthNode = {name: 'Timmy', next: 317}
+let fifthNode = {name: 'Jacob', next: null}
+
+let collection = {0:firstNode, 108:secondNode,
+  917:thirdNode, 389:fourthNode, 317:fifthNode}
+
+function getName(node){
+  return node["name"]
+}
+
+function headNode(linkedList, collection){
+  return collection[linkedList]
+}
+
+function next(node, collection){
+  return collection[node["next"]]
+}
+
+function nodeAt(index, linkedList, collection){
+  let head=collection[linkedList]
+  let nodeToReturn=head
+  for (let i=0; i<index; i++){
+    nodeToReturn = next(nodeToReturn, collection)
+  }
+  return nodeToReturn
+}
+
+function addressAt(index, linkedList, collection){
+  if (index==0){
+    return linkedList
+  } else {
+  let nodeThatPointsToIndex=nodeAt(index-1, linkedList, collection)
+  return nodeThatPointsToIndex["next"]
+  }
+}
+
+function indexAt(targetNode, collection, linkedList){
+  let currentNode=collection[linkedList]
+  let i=0
+  while (currentNode!=targetNode){
+    i++
+    currentNode=next(currentNode, collection)
+  }
+  return i
+}
